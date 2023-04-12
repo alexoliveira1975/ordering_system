be284be215787f86a0d06c4a7f89e26f7d4fdccb
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-04-12 16:12:55 -0300

    Product

---
+++ src/main/java/com/adhocsolucoes/ordering_system/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.adhocsolucoes.ordering_system.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adhocsolucoes.ordering_system.entities.Product;
+import com.adhocsolucoes.ordering_system.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
