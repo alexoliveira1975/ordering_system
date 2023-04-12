dbf365b180e31c0e9d5253578761efc73c509aad
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-04-12 13:04:53 -0300

    Service layer, component registration

---
+++ src/main/java/com/adhocsolucoes/ordering_system/services/UserService.java
@@ -0,0 +1,27 @@
+package com.adhocsolucoes.ordering_system.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adhocsolucoes.ordering_system.entities.User;
+import com.adhocsolucoes.ordering_system.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
