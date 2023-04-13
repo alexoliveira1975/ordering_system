e1188ce33b4d477c2d69d698cf30b8e4578bbb68
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-04-13 19:23:16 -0300

    Exception handling - findById

--- src/main/java/com/adhocsolucoes/ordering_system/services/UserService.java
+++ src/main/java/com/adhocsolucoes/ordering_system/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.adhocsolucoes.ordering_system.entities.User;
 import com.adhocsolucoes.ordering_system.repositories.UserRepository;
+import com.adhocsolucoes.ordering_system.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
