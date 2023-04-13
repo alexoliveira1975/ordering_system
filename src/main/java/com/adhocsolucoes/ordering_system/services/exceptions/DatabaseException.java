418bc38e2b1beb855b5b05566422c2f947203acc
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-04-13 20:24:45 -0300

    Exception handling - delete

---
+++ src/main/java/com/adhocsolucoes/ordering_system/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.adhocsolucoes.ordering_system.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
