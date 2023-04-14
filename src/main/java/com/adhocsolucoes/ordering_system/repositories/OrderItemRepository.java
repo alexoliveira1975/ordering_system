7c692b876482ef57f1eaf26c65ce693349ecbed1
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-04-14 10:39:42 -0300

    Dev profile

--- src/main/java/com/adhocsolucoes/ordering_system/repositories/OrderItemRepository.java
+++ src/main/java/com/adhocsolucoes/ordering_system/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.adhocsolucoes.ordering_system.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.adhocsolucoes.ordering_system.entities.OrderItem;
+import com.adhocsolucoes.ordering_system.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
